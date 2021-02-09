8c7946d77043c446282c362fa34aa825e2e3900a
seoyeon <silversun0808@gmail.com>
2021-02-10 01:25:30 +0900

    create LoadingActivity add yellow_light color

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //start Loading Activity
+        Intent intent = new Intent(this, LoadingActivity.class);
+        startActivity(intent);
     }
+
 }
