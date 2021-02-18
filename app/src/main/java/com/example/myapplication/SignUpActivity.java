21eae83c0e728b099d7cc328c282b5f928bd8113
seoyeon <silversun0808@gmail.com>
2021-02-18 17:15:29 +0900

    define firebase,View object initializing Views

--- app/src/main/java/com/example/myapplication/SignUpActivity.java
+++ app/src/main/java/com/example/myapplication/SignUpActivity.java
@@ -5,18 +5,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignUpActivity extends Activity {
+
+    //define firebase object
+    FirebaseAuth firebaseAuth;
+
+    //define View object
+    EditText signUp_name;
+    EditText signUp_age;
+    EditText signUp_ID;
+    EditText signUp_PW;
+    EditText signUp_PW2;
+    Button signUp_btn_signUp;
+    Button signUp_btn_back;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        //initializing Views
+        signUp_name = (EditText) findViewById(R.id.signUp_name);
+        signUp_age = (EditText) findViewById(R.id.signUp_age);
+        signUp_ID = (EditText) findViewById(R.id.signUp_ID);
+        signUp_PW = (EditText) findViewById(R.id.signUp_PW);
+        signUp_PW2 = (EditText) findViewById(R.id.signUp_PW2);
+        signUp_btn_signUp = (Button) findViewById(R.id.signUp_btn_signUp);
+        signUp_btn_back = (Button) findViewById(R.id.signUp_btn_back);
+
         //back to Login button Activity
-        Button signUp_btn_back = (Button) findViewById(R.id.signUp_btn_back);
         signUp_btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,5 +48,18 @@ public class SignUpActivity extends Activity {
                 startActivity(intent);
             }
         });
+
+        //initializing firebase auth object
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //if already login
+        if(firebaseAuth.getCurrentUser() != null){
+            finish();
+        }
+    }
+
+    //Firebase creating a new user
+    private void createUser(){
+
     }
 }
