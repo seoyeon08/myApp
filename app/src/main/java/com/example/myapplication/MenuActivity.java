857315c18f384fccbe0bc34017678f5220afc766
seoyeon <silversun0808@gmail.com>
2021-02-22 19:48:57 +0900

    add bottom_nav_view

--- app/src/main/java/com/example/myapplication/MenuActivity.java
+++ app/src/main/java/com/example/myapplication/MenuActivity.java
@@ -1,10 +1,15 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MenuActivity extends AppCompatActivity {
     private FragmentManager fragmentManager = getSupportFragmentManager();
@@ -16,5 +21,28 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout, gpsFragment).commitAllowingStateLoss();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction transaction1 = fragmentManager.beginTransaction();
+                switch (item.getItemId()) {
+                    case R.id.gpsItem:
+                        transaction.replace(R.id.frameLayout, gpsFragment).commitAllowingStateLoss();
+                        break;
+                    case R.id.mapItem:
+                        transaction.replace(R.id.frameLayout, mapFragment).commitAllowingStateLoss();
+                        break;
+                    case R.id.bookmarkItem:
+                        transaction.replace(R.id.frameLayout, bookmarkFragment).commitAllowingStateLoss();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
