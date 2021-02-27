5add80587ce10da9ce5ca787b6c029318ee68c2b
seoyeon <silversun0808@gmail.com>
2021-02-27 23:04:17 +0900

    main 화면 button 구현

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,14 +16,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //바로가기 버튼 만들기
-        Intent intent = new Intent(this, MapFragment.class);
+        Button calculateBtn = (Button) findViewById(R.id.main_calculate);
+        Button placeBtn = (Button) findViewById(R.id.main_place);
 
-        Button calculateBtn  = (Button)findViewById(R.id.main_calculate);
+        //main calculate button Activity
         calculateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MapFragment.class);
+                startActivity(intent);
+            }
+        });
+
+        //main place(bookmark) button Activity
+        placeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), BookmarkFragment.class);
+                startActivity(intent);
             }
         });
     }
