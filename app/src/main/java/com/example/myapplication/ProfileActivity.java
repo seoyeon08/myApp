4c03740515aaf88ddd60eb6b2f38e01e4d2f589b
seoyeon <silversun0808@gmail.com>
2021-03-03 23:26:42 +0900

    ProfileActivity onCreate부분 작성

--- app/src/main/java/com/example/myapplication/ProfileActivity.java
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,9 +11,10 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth firebaseAuth;
 
@@ -24,7 +26,32 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        //initializing Views
+        profile_UserEmail = (TextView) findViewById(R.id.profile_UserEmail);
+        buttonLogout = (Button) findViewById(R.id.profile_btn_logout);
+        buttonDelete = (Button) findViewById(R.id.profile_btn_signOut);
+
+        //initializing firebase authentication object
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() == null){
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        profile_UserEmail.setText("안녕하세요.\n"+user.getEmail()+"으로 로그인 하였습니다.");
+
+        //logout button event
+        buttonLogout.setOnClickListener(this);
+        buttonDelete.setOnClickListener(this);
     }
 
 
+    @Override
+    public void onClick(View v) {
+
+    }
 }
