c645c062adbeecf896ff44330fa4928bf43abde1
seoyeon <silversun0808@gmail.com>
2021-03-04 23:50:39 +0900

    ProfileActivity onClick부분 작성

--- app/src/main/java/com/example/myapplication/ProfileActivity.java
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -1,17 +1,25 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import static android.widget.Toast.LENGTH_LONG;
 
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
@@ -52,6 +60,35 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
     @Override
     public void onClick(View v) {
+        if(v == buttonLogout){
+            firebaseAuth.signOut();
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
 
+        if(v == buttonDelete){
+            AlertDialog.Builder alert_confirm = new AlertDialog.Builder(ProfileActivity.this);
+            alert_confirm.setMessage("정말 계정을 삭제하시겠습니까?").setCancelable(false).setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Toast.makeText(ProfileActivity.this,"계정이 삭제되었습니다.", Toast.LENGTH_LONG).show();
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), LoadingActivity.class));
+                        }
+                    });
+                }
+            });
+            alert_confirm.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(ProfileActivity.this, "취소했습니다.", Toast.LENGTH_LONG).show();
+                }
+            });
+            alert_confirm.show();
+        }
     }
 }
